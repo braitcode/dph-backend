ad46c1cdb8e183ea886a2cdfb8817187df9929de
Bright Okhumale <156666393+braitcode@users.noreply.github.com>
2024-09-02 07:44:50 +0100

    Development (#11)
    * readme
    
    * second commit
    
    * Brait (#4)
    
    * new commit
    
    * delete
    
    * Brait (#5)
    
    * new commit
    
    * delete
    
    * Brait (#7)
    
    * new commit
    
    * delete
    
    * cloudinary
    
    * Brait (#9)
    
    * new commit
    
    * delete
    
    * cloudinary
    
    * install
    
    * mongo

--- db.config.js
+++ db.config.js
@@ -1,7 +1,13 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
 export const connectDB = (url) => {
-    mongoose
-    .connect(url).then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log('Error connecting to MongoDB', err.message))
-}
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // Increase server selection timeout to 30 seconds
+      socketTimeoutMS: 45000, // Set socket timeout to 45 seconds
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('Error connecting to MongoDB', err.message));
+};
