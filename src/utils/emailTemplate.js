579f72bb611459e1e5cd3cd418ac63b0038ee577
Bright Okhumale <156666393+braitcode@users.noreply.github.com>
2024-09-06 23:08:52 +0100

    Development (#33)
    * readme
    
    * second commit
    
    * Brait (#4)
    
    * new commit
    
    * delete
    
    * Brait (#5)
    
    * new commit
    
    * delete
    
    * Brait (#7)
    
    * new commit
    
    * delete
    
    * cloudinary
    
    * Brait (#9)
    
    * new commit
    
    * delete
    
    * cloudinary
    
    * install
    
    * mongo
    
    * cors
    
    * new
    
    * Brait (#21)
    
    * new commit
    
    * delete
    
    * cloudinary
    
    * install
    
    * Brait (#23)
    
    * new commit
    
    * delete
    
    * cloudinary
    
    * install
    
    * cors
    
    * new cors
    
    * Brait (#25)
    
    * new commit
    
    * delete
    
    * cloudinary
    
    * install
    
    * cors
    
    * new cors
    
    * Brait (#27)
    
    * new commit
    
    * delete
    
    * cloudinary
    
    * install
    
    * cors
    
    * new cors
    
    * email
    
    * Brait (#29)
    
    * new commit
    
    * delete
    
    * cloudinary
    
    * install
    
    * cors
    
    * new cors
    
    * email
    
    * template
    
    * Brait (#30)
    
    * new commit
    
    * delete
    
    * cloudinary
    
    * install
    
    * cors
    
    * new cors
    
    * email
    
    * template
    
    * email
    
    * email
    
    * Brait (#32)
    
    * new commit
    
    * delete
    
    * cloudinary
    
    * install
    
    * cors
    
    * new cors
    
    * email
    
    * template
    
    * email
    
    * email
    
    * email template

--- src/utils/emailTemplate.js
+++ src/utils/emailTemplate.js
@@ -99,119 +99,174 @@ export const PASSWORD_RESET_REQUEST_TEMPLATE = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reset Your Password</title>
     <link rel="stylesheet" href="../new.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
-<style>
-body {
-    margin: 0;
-    padding: 0;
-}
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
 
-.container{
-    background-color: #EEFFF7;
-    width: 700px;
-    height: 617px;
-}
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Space Grotesk', sans-serif;
+            background-color: #EEFFF7;
+        }
 
-.logo{
-    background-color: #1E3A2D;
-    width: 100%;
-    height: 160px;
-}
-.new{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 24px;
-}
+        .container {
+            background-color: #EEFFF7;
+            width: 90%;
+            max-width: 700px;
+            margin: 0 auto;
+            padding: 20px;
+            border-radius: 10px;
+        }
 
-.header p{
-    display: flex;
-    justify-content: center;
-    font-size: 35px;
-    font-weight: bold;
-    font-family: "Space Grotesk", sans-serif;
-}
-.email-body{
-    width: 620px;
-    height: 124px;
-    font-size: 24px;
-    line-height: 30.62px;
-}
-.email-body p{
-    text-align: center;
-    font-family: "Space Grotesk", sans-serif;
-}
-.reset-btn{
-    background-color: #028A4C;
-    border: none;
-    width: 433px;
-    height: 40px;
-    color: white;
-    border-radius: 4.9px;
-    font-family: "Space Grotesk", sans-serif;
-    cursor: pointer;
-}
-.icons{
-     width: 320px;
-     display: flex;
-     gap: 29.23px;
-}
+        .logo {
+            background-color: #1E3A2D;
+            padding: 20px;
+            text-align: center;
+        }
+        .logo-div{
+          display: flex;
+           align-items: center; 
+           justify-content: center;
+        }
 
-</style>
+        .logo img {
+            width: 46.69px;
+            height: 46.69px;
+        }
+
+        .logo h1 {
+            font-size: 2.5rem;
+            color: white;
+            margin: 10px 0;
+        }
+
+        .logo p {
+            color: white;
+            font-size: 1rem;
+            margin: 0;
+        }
+
+        .email-body {
+            text-align: center;
+            padding: 20px;
+            font-size: 1.2rem;
+            line-height: 1.6;
+        }
+
+        .email-body p {
+            margin: 0 0 20px;
+            color: #333;
+        }
+
+        .reset-btn {
+            background-color: #028A4C;
+            color: white;
+            padding: 12px 20px;
+            border: none;
+            border-radius: 5px;
+            font-size: 1rem;
+            cursor: pointer;
+            text-decoration: none;
+            display: inline-block;
+        }
+
+        .reset-btn:hover {
+            background-color: #026f39;
+        }
+
+        .icons {
+            display: flex;
+            justify-content: center;
+            gap: 15px;
+            margin: 20px 0;
+        }
+
+        .icons img {
+            width: 35px;
+            height: 35px;
+        }
+        .email-footer{
+          text-align: center; 
+          font-size: 0.9rem;
+          margin-top: 20px;
+        }
+        .email-footer img{
+          width: 20px;
+          height: 20px;
+        }
+
+        @media (max-width: 768px) {
+            .container {
+                padding: 15px;
+            }
+
+            .logo h1 {
+                font-size: 2rem;
+            }
+
+            .email-body {
+                font-size: 1rem;
+            }
+
+            .reset-btn {
+                width: 100%;
+            }
+
+            .icons img {
+                width: 30px;
+                height: 30px;
+            }
+        }
+
+        @media (max-width: 480px) {
+            .email-body {
+                font-size: 0.9rem;
+            }
+
+            .icons img {
+                width: 28px;
+                height: 28px;
+            }
+        }
+    </style>
 </head>
 <body>
     <div class="container">
-        <div class="logo" style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
-            <div class="" style="display: flex; align-items: center; height: 60px;">
-                <img src="./images/Group 1.png" alt="" style="width: 46.69px; height: 46.69px;">
-                <h1 style="font-size: 49px;color: white;">DPH</h1>
+        <div class="logo">
+            <div class="logo-div">
+                <img src="./images/Group 1.png" alt="Logo">
+                <h1>DPH</h1>
             </div>
-            <p style="color: white; font-family: Space Grotesk;">DIGITAL PRESENCE HUB</p>
+            <p>DIGITAL PRESENCE HUB</p>
         </div>
-        <div class="email">
-            <div class="new">
-                <div class="header" style="height: 57px;">
 
+        <div class="email-body">
+            <h2>Hi, {firstName}</h2>
+            <p>By resetting your password, you'll also confirm your email associated with the account. If you didn't request this reset, you can safely ignore this email.</p>
+            <a href="{resetLink}" class="reset-btn">Reset Password</a>
 
-                    <p>Hi, {firstName}</p>
-
-                </div>
-                <div class="email-body">
-    
-                    <p>By resetting your password, you'll also confirm your email associated with the account. if you didn't request this reset, you can safely ignore this emial.</p>
-                </div>
-                <div class="">
-                    <a href="{resetLink}"><button class="reset-btn">Reset Password</button></a>
-                </div>
-                <div class="icons">
-                    <a href="#"><img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725647630/twitter_e8el6s.png" style="width: 38.46px; height: 38.46px;" alt="Twitter"></a>
-                    <a href="#"><img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725647885/youtube_mx48xs.png" style="width: 38.46px; height: 38.46px;" alt="YouTube"></a>
-                    <a href="#"><img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725647884/instagram_bs7157.png" style="width: 38.46px; height: 38.46px;" alt="Instagram"></a>
-                    <a href="#"><img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725647884/linkedin_jdppgk.png" style="width: 38.46px; height: 38.46px;" alt="LinkedIn"></a>
-                    <a href="#"><img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725647884/facebook_i3cu6z.png" style="width: 38.46px; height: 38.46px;" alt="Facebook"></a>
-                </div>
-                <p style="display: flex; gap: 5px; font-family: Space Grotesk;">
-                    <a href="#"><img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725649485/copyright-regular_jeqo6m.svg" style="width: 20.46px; height: 20.46px;" alt=""></a> 2024 DPH Inc. All rights reserved.
-                </p>
-                
-            </div>
-        </div>
-        <!-- <div class="text-body">
-        </div> -->
-        
-       
-        <div class="">
 
         </div>
+
+        <div class="icons">
+            <a href="#"><img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725647630/twitter_e8el6s.png" alt="Twitter"></a>
+            <a href="#"><img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725647885/youtube_mx48xs.png" alt="YouTube"></a>
+            <a href="#"><img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725647884/instagram_bs7157.png" alt="Instagram"></a>
+            <a href="#"><img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725647884/linkedin_jdppgk.png" alt="LinkedIn"></a>
+            <a href="#"><img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725647884/facebook_i3cu6z.png" alt="Facebook"></a>
+        </div>
+
+        <p class="email-footer">
+            <img src="https://res.cloudinary.com/dww4lgcy9/image/upload/v1725649485/copyright-regular_jeqo6m.svg" alt=""> 
+            2024 DPH Inc. All rights reserved.
+        </p>
     </div>
-
 </body>
 </html>
 `;
