0c650249936fdbb3f05085d5dc78edbf3c0a4a9c
braitcode <brightjohn489@gmail.com>
2024-09-10 14:19:41 +0100

    newsletter

--- index.js
+++ index.js
@@ -4,12 +4,44 @@ import cors from "cors";
 import { connectDB } from "./db.config.js";
 import authRouter from "./src/routes/auth.js";
 import userRouter from "./src/routes/user.js";
+// import { google } from "googleapis";
+import newsletterRouter from "./src/routes/newsletterEmail.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 const dbUrl = process.env.MONGODB_URL;
+
+// const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+// const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+// const REDIRECT_URI = process.env.REDIRECT_URI;
+
+// const oauth2Client = new google.auth.OAuth2(
+//   CLIENT_ID,
+//   CLIENT_SECRET,
+//   REDIRECT_URI
+// );
+
+// // Route to handle Google OAuth login
+// app.get('/auth/google', (req, res) => {
+//   const scopes = ['https://www.googleapis.com/auth/userinfo.profile'];
+//   const url = oauth2Client.generateAuthUrl({
+//     access_type: 'offline',
+//     scope: scopes,
+//   });
+//   res.redirect(url);
+// });
+
+// // Route to handle Google OAuth callback
+// app.get('/auth/google/callback', async (req, res) => {
+//   const code = req.query.code;
+//   const { tokens } = await oauth2Client.getToken(code);
+//   oauth2Client.setCredentials(tokens);
+
+//   // Do something with the tokens (e.g., get user info)
+//   res.send('Google OAuth successful');
+// });
 
 // Connect to MongoDB
 connectDB(dbUrl);
@@ -34,6 +66,7 @@ app.get('/', (req, res) => {
 // Routers
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+app.use("/api/newsletter", newsletterRouter);
 
   app.listen(port, (req, res) => {
     console.log(`Server running on port ${port}`)
