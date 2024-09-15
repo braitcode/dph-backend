7a08735908b27ce76bfa12be3782469b97149f05
braitcode <brightjohn489@gmail.com>
2024-09-15 18:53:52 +0100

    jwt

--- src/routes/googleRoute.js
+++ src/routes/googleRoute.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
-  const { token, user } = req.user;
+    const token = jwt.sign({ user: req.user }, process.env.JWT_SECRET, { expiresIn: '1d' });
   // Send user information as a query parameter
   res.redirect(`https://dph-frontend.vercel.app/oauth-callback?token=${token}&user=${encodeURIComponent(JSON.stringify(user))}`);
 });
