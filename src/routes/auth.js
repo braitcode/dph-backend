1a81fa699c5d7a082d3fe79afa27fda0cf481d5b
braitcode <brightjohn489@gmail.com>
2024-09-15 18:49:27 +0100

    auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { forgotPassword, login, resetPassword, signUp } from '../controllers/auth.js';
 import { upload } from '../helpers/multer.js';
-import passport from 'passport';
+// import passport from 'passport';
 
 const router = express.Router();
 
@@ -10,12 +10,12 @@ router.post("/signup", upload.single('image'), signUp);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:resetToken", resetPassword);
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+// router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
-  const { token, user } = req.user;
-  // Send user information as a query parameter
-  res.redirect(`https://dph-frontend.vercel.app/oauth-callback?token=${token}&user=${encodeURIComponent(JSON.stringify(user))}`);
-});
+// router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
+//   const { token, user } = req.user;
+//   // Send user information as a query parameter
+//   res.redirect(`https://dph-frontend.vercel.app/oauth-callback?token=${token}&user=${encodeURIComponent(JSON.stringify(user))}`);
+// });
 
 export default router
