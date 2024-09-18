d7dc1063eec06e2e6badd4e4f0a2bb810e012404
braitcode <brightjohn489@gmail.com>
2024-09-18 02:19:49 +0100

    awe

--- src/controllers/newsletter.js
+++ src/controllers/newsletter.js
@@ -18,7 +18,10 @@ export const subscribeToNewsletter = async (req, res) => {
           return res.status(404).json({success:false, message: "User not found"});
       }
 
-     
+      const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "Email is taken" });
+    }
 
     // Basic email format validation (you can add more complex validation)
     const emailRegex = /\S+@\S+\.\S+/;
@@ -27,11 +30,6 @@ export const subscribeToNewsletter = async (req, res) => {
     }
 
     try {
-
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-          return res.status(400).json({ error: "Email is taken" });
-        }
         // Save subscriber to database (or any storage system)
         // await saveSubscriber({ email, firstName });
 
