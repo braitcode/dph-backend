b7624a04a9379b62257c8096f5d3a310f2b02542
braitcode <brightjohn489@gmail.com>
2024-09-18 16:21:27 +0100

    nnew

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(
   cookieSession({
     name: "session",
     keys: ["cyberwolve"],
-    maxAge: 24 * 60 * 60 * 100, // 24 hours
+    maxAge: 24 * 60 * 60 * 100,
   })
 )
 // Initialize Passport
