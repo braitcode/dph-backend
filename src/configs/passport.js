de5c4f81fdc24615d96792a1bed298f6a18f0496
braitcode <brightjohn489@gmail.com>
2024-09-19 10:07:46 +0100

    new

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -10,7 +10,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://dph-backend.onrender.com/api/auth/google/callback'
+  callbackURL: '/api/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   const { id, name, emails } = profile;
   try {
